e9ad45ed4b50dcc2aa8d10abe57cc061ebd206c9
abhishekj123 <abhishekjayalal007@gmail.com>
2022-05-14 00:11:00 +0530

    facebook style main menu completed

--- components/MainmenuComponent/Mainmenu.jsx
+++ components/MainmenuComponent/Mainmenu.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState , useRef } from 'react'
 import { MenuitemCard , Menusubpanel, Mainmenudropdown } from '../../components/components' 
+import { CSSTransition } from 'react-transition-group';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft } from "@fortawesome/free-regular-svg-icons"
+
+
 
 const Mainmenu = (props) => {
 
@@ -14,29 +19,18 @@ const Mainmenu = (props) => {
   }
 ]  
 
-    let menuItem=["Shawarma               "
-    ,"Hot Treats             "
-    ,"ZAM ZAM COMBOS         "
-    ,"Fried Rice Combo       "
-    ,"Fried Rice             "
-    ,"Starters               "
-    ,"Zam Zam Digesters      "
-    ,"Zam Zam Lassi          "
-    ,"RAMADAN SNACKS         "
-    ,"RAMADAN COMBO          "
-    ,"Breakfast              "
-    ,"Breakfast Curry        "
-    ,"Breakfast Combo        "]
+   
 
   return (
     <div className='Mainmenu'>
     <div className='menu-panel-1'>
-        {
-          option.map(item => 
-              <Mainmenudropdown subTitle={item.title} subOptions={item.subOption}/>
-                
-                )
-        }
+    
+    
+    <div className='drop-down-outer'>
+    
+    <DropdownMenu/>
+    </div>
+       
     </div>
     <div className='menu-panel-2'>
     {
@@ -60,3 +54,96 @@ const Mainmenu = (props) => {
 }
 
 export default Mainmenu
+
+
+
+const DropdownMenu = () => {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  const calcHeight=(el) =>{
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  const DropdownItem= (props) => {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    )
+
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+    <CSSTransition
+    in={activeMenu === 'main'}
+    timeout={500}
+    classNames="menu-primary"
+    unmountOnExit
+    onEnter={calcHeight}>
+    <div className="menu">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        
+       
+        goToMenu="settings">
+        Veg Starters
+      </DropdownItem>
+      <DropdownItem
+        
+       
+      goToMenu="settings">
+      Non Veg Starters
+    </DropdownItem>
+      <DropdownItem
+        
+       
+        goToMenu="settings">
+        Veg Main 
+      </DropdownItem>
+      <DropdownItem
+        leftIcon="🦧"
+        
+        goToMenu="animals">
+        Non Veg Main
+      </DropdownItem>
+      <DropdownItem
+        leftIcon="🦧"
+        goToMenu="animals">
+        Deserts
+      </DropdownItem>
+      <DropdownItem
+        leftIcon="🦧"
+        goToMenu="animals">
+        Beverages
+      </DropdownItem>
+
+    </div>
+  </CSSTransition>
+
+  <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<FontAwesomeIcon icon={faCircleLeft} size="2x" className="highlight" />}>
+            Go Back
+          </DropdownItem>
+          <DropdownItem leftIcon='doosh'>HTML</DropdownItem>
+          <DropdownItem leftIcon='doosh'>CSS</DropdownItem>
+          <DropdownItem leftIcon='doosh'>JavaScript</DropdownItem>
+          <DropdownItem leftIcon='doosh'>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+    
+    </div>
+    
+  )
+}
